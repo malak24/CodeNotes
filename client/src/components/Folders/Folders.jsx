e397407f47815dfda913c857eb7bb955a92fba41
malak kataie <malakkataie@gmail.com>
2020-09-01 21:33:51 -0400

    Added the font to App.scss, imported variables and mixins and created a non functional sideBar

--- client/src/components/Folders/Folders.jsx
+++ client/src/components/Folders/Folders.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './Folders.scss';
-import folder from '../../assets/folder.png'
+import folders from '../../assets/folders.png'
 import files from '../../assets/files.png'
 import search from '../../assets/search.png'
 import customize from '../../assets/customize.png'
@@ -12,12 +12,12 @@ class Folders extends Component {
   render() {
     return (
       <div className = 'sideBar'>
-        <img src = {folder} alt = 'folder icon'/>
-        <img src = {files} alt = 'files icon'/>
-        <img src = {search} alt = 'search icon'/>
-        <img src = {customize} alt = 'folder icon'/>
-        <img src = {cheatsheet} alt = 'folder icon'/>
-        <img src = {zen} alt = 'folder icon'/>
+        <img className = 'sideBar__icon' src = {folders} alt = 'folder icon'/>
+        <img className = 'sideBar__icon' src = {files} alt = 'files icon'/>
+        <img className = 'sideBar__icon' src = {search} alt = 'search icon'/>
+        <img className = 'sideBar__icon' src = {customize} alt = 'folder icon'/>
+        <img className = 'sideBar__icon' src = {cheatsheet} alt = 'folder icon'/>
+        <img className = 'sideBar__icon' src = {zen} alt = 'folder icon'/>
       </div>
     )
   }
