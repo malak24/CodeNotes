9c0b7e97be701f6af9e2ec6bcc7cb0648ac947ec
malak kataie <malakkataie@gmail.com>
2020-09-06 13:34:22 -0400

    Added Froala library

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,25 @@ import './App.scss';
 import SideBar from './components/SideBar/SideBar'
 import Folders from './components/Folders/Folders'
 import Files from './components/Files/Files'
-import Topbar from './components/Topbar/Topbar'
-import Main from './components/Main/Main'
+// import Topbar from './components/Topbar/Topbar'
+// import Main from './components/Main/Main'
+
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import FroalaEditorComponent from './froala/lib/FroalaEditor';
+import 'froala-editor/js/plugins.pkgd.min.js';
+import 'froala-editor/js/languages/de.js';
+
+import 'froala-editor/js/third_party/image_tui.min.js';
+import 'froala-editor/js/third_party/embedly.min.js';
+import 'froala-editor/js/third_party/spell_checker.min.js';
+
+
+// import FroalaEditorView from './froala/lib/FroalaEditorView';
+// import FroalaEditorA from './froala/lib/FroalaEditorA';
+// import FroalaEditorButton from './froala/lib/FroalaEditorButton';
+// import FroalaEditorImg from './froala/lib/FroalaEditorImg';
+// import FroalaEditorInput from './froala/lib/FroalaEditorInput';
 
 
 function App() {
@@ -14,8 +31,8 @@ function App() {
       <Folders />
       <Files />
       <div className='app__right-section'>
-        <Topbar />
-        <Main />
+        {/* <Topbar /> */}
+        <FroalaEditorComponent className = 'app__editor' tag = 'textarea' />
       </div>
     </div>
   );
