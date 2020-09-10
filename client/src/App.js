b2b5933038abbfd05c1b932731535b0fb4cdcb25
malak kataie <malakkataie@gmail.com>
2020-09-10 18:18:24 -0400

    Setting up the backend basic structure, finished toggling files and folders sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import SideBar from './components/SideBar/SideBar'
 import Folders from './components/Folders/Folders'
 import Files from './components/Files/Files'
 import Editor from './components/Editor/Editor'
-// import Topbar from './components/Topbar/Topbar'
-// import Main from './components/Main/Main'
 
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
@@ -17,13 +17,6 @@ import 'froala-editor/js/languages/de.js';
 import 'froala-editor/js/third_party/image_tui.min.js';
 import 'froala-editor/js/third_party/embedly.min.js';
 import 'froala-editor/js/third_party/spell_checker.min.js';
-
-
-// import FroalaEditorView from './froala/lib/FroalaEditorView';
-// import FroalaEditorA from './froala/lib/FroalaEditorA';
-// import FroalaEditorButton from './froala/lib/FroalaEditorButton';
-// import FroalaEditorImg from './froala/lib/FroalaEditorImg';
-// import FroalaEditorInput from './froala/lib/FroalaEditorInput';
 
 
 class App extends Component {
@@ -59,10 +52,11 @@ class App extends Component {
         toggleFolders = {this.state.toggleFolders} 
         toggleFiles = {this.state.toggleFiles} 
         foldersClick = {this.foldersClick}
-        filesClick = {this.filesClick}/>
+        filesClick = {this.filesClick}
+        />
 
         <Folders toggleFolders = {this.state.toggleFolders} foldersClick = {this.foldersClick}/>
-        <Files toggleFiles = {this.state.toggleFolders} filesClick = {this.filesClick}/>
+        <Files toggleFiles = {this.state.toggleFiles} filesClick = {this.filesClick}/>
         <div className='app__right-section'>
           <Editor />
         </div>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = 8080;
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log('app is running on port 8080')
