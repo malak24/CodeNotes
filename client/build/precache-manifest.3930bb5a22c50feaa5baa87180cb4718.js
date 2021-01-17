29260dccb6efc38ddc2c69fa89492f7805a2b080
EC2 Default User <ec2-user@ip-172-31-22-70.us-east-2.compute.internal>
2021-01-17 22:43:09 +0000

    Adding the last functional version of CodeNotes from the EC2 instance (adding notes to a folder is now functional)

---
+++ client/build/precache-manifest.3930bb5a22c50feaa5baa87180cb4718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b359e14c113d9f0b7660b32a2eb94b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcadea8b15cdffd9e0c7",
+    "url": "/static/css/2.69e24d06.chunk.css"
+  },
+  {
+    "revision": "5f549ca2d4cf6831296d",
+    "url": "/static/css/main.9acacc58.chunk.css"
+  },
+  {
+    "revision": "bcadea8b15cdffd9e0c7",
+    "url": "/static/js/2.d26047a7.chunk.js"
+  },
+  {
+    "revision": "5d7d7178bab1f227e0d71f061393633b",
+    "url": "/static/js/2.d26047a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f549ca2d4cf6831296d",
+    "url": "/static/js/main.e3f889c9.chunk.js"
+  },
+  {
+    "revision": "ea217a63be6b7e135b37",
+    "url": "/static/js/runtime-main.9433b148.js"
+  }
+]);
