3f01e489da3209e1b090be5b5c5a256096f33ce1
Malak Kataie <malakkataie@gmail.com>
2021-06-21 16:37:08 -0400

    Fixing 'cannot read property cors of undefined' error

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const PORT = 8080;
 const app = express();
 const route = require('./connect');
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/", route);
